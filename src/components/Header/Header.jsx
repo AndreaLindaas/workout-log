53cba5f5758a5d4732be57d210c1669ef16b1b5e
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-01-08 20:31:10 +0100

    added header and menu

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,58 @@
+import "./Header.scss";
+import { Drawer, useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const isDesktop = useMediaQuery("(min-width:768px)");
+
+  const menuClose = () => {
+    setMenuOpen(false);
+  };
+  const toggleMenu = () => {
+    if (menuOpen) {
+      setMenuOpen(false);
+    } else {
+      setMenuOpen(true);
+    }
+  };
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+  const renderMenuItems = () => {
+    return (
+      <ul className="headerList">
+        <li>
+          <Link to="/">
+            <span>Home</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <span>Other</span>
+          </Link>
+        </li>
+      </ul>
+    );
+  };
+  if (!isDesktop) {
+    return (
+      <>
+        <div className="menuIcon" onClick={toggleMenu}>
+          <MenuIcon />
+        </div>
+        <Drawer
+          anchor="top"
+          open={menuOpen}
+          onClose={menuClose}
+          onClick={closeMenu}
+        >
+          {renderMenuItems()}
+        </Drawer>
+      </>
+    );
+  } else {
+    return <>{renderMenuItems()}</>;
+  }
+}
