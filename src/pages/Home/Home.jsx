abe924b223dbfe7ef7284bdbb24adf8ad12da195
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-01-10 21:30:22 +0100

    made a function to redirect and save user

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,37 @@
 import { useEffect, useState } from "react";
 import { BASE_URL } from "../../lib/constants";
 import "./Home.scss";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch(`${BASE_URL}/users/`)
       .then((response) => response.json())
       .then((result) => {
-        console.log(result);
         setUsers(result);
       });
   }, []);
 
+  const saveUserAndRedirect = (event) => {
+    const user = users.filter((user) => user.id === event.target.dataset.id);
+
+    localStorage.setItem("user", JSON.stringify(user[0]));
+    navigate("/dashboard/" + event.target.dataset.id);
+  };
+
   const showUsers = () => {
     return users.map((user) => (
-      <Link to={`/dashboard/${user.id}/`} key={user.id}>
-        <div className="users">{user.name}</div>
-      </Link>
+      <div
+        key={user.id}
+        data-id={user.id}
+        onClick={saveUserAndRedirect}
+        className="users"
+      >
+        {user.name}
+      </div>
     ));
   };
 
