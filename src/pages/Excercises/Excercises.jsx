f330cc019f61c0cd30c7046299d3bafbfa3b51ac
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-01-11 11:55:45 +0100

    added card styling and image to excercise page

--- src/pages/Excercises/Excercises.jsx
+++ src/pages/Excercises/Excercises.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { BASE_URL } from "../../lib/constants";
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import "./Excercises.scss";
 export default function Excercises() {
   const [excercises, setExcercises] = useState([]);
   useEffect(() => {
@@ -13,9 +15,23 @@ export default function Excercises() {
   const seeExcercises = () => {
     return excercises.map((excercice) => {
       return (
-        <div key={excercice.id}>
-          <span>{excercice.name}</span>
-        </div>
+        <>
+          <div key={excercice.id}>
+            <span></span>
+          </div>
+
+          <Card sx={{ maxWidth: 345 }} className="muiCard">
+            <CardMedia
+              sx={{ height: 140 }}
+              image="./assets/media/trening.jpg"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {excercice.name}
+              </Typography>
+            </CardContent>
+          </Card>
+        </>
       );
     });
   };
