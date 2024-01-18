f061bcd919f4bab8fc5cebb017927d9f23bd4da3
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-01-18 21:17:25 +0100

    made create excercise and error handling

--- src/pages/CreateExcercise/CreateExcercise.jsx
+++ src/pages/CreateExcercise/CreateExcercise.jsx
@@ -6,13 +6,38 @@ import {
   Button,
 } from "@mui/material";
 import User from "../../components/User/User";
-// import PocketBase from "pocketbase";
-import { useEffect } from "react";
-
+import PocketBase from "pocketbase";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function CreateExcercise() {
-  // const pb = new PocketBase("https://trening.pockethost.io");
-  const submitExcerciseForm = (event) => {
+  const pb = new PocketBase("https://trening.pockethost.io");
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [excerciseName, setExcerciseName] = useState("");
+  const navigate = useNavigate();
+
+  const submitExcerciseForm = async (event) => {
     event.preventDefault();
+    setButtonDisabled(true);
+
+    const data = {
+      name: excerciseName,
+    };
+    try {
+      await pb.collection("excercises").create(data);
+      navigate("/register-performance");
+    } catch (error) {
+      console.log(error);
+      setButtonDisabled(false);
+    }
+  };
+
+  const nameChanged = (event) => {
+    setExcerciseName(event.target.value);
+    if (event.target.value.length > 1) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
   };
 
   return (
@@ -22,6 +47,9 @@ export default function CreateExcercise() {
         <div>
           <FormControl variant="filled" className="form">
             <FilledInput
+              name="excercise"
+              value={excerciseName}
+              onChange={nameChanged}
               endAdornment={<InputAdornment position="end"> </InputAdornment>}
               aria-describedby="filled-weight-helper-text"
               inputProps={{
@@ -33,7 +61,12 @@ export default function CreateExcercise() {
             </FormHelperText>
           </FormControl>
         </div>
-        <Button type="submit" variant="contained" className="button">
+        <Button
+          disabled={buttonDisabled}
+          type="submit"
+          variant="contained"
+          className="button"
+        >
           Save
         </Button>
       </form>
